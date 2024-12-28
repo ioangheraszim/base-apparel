2df8c4962971f13abd4810233b4277b926a86e27
ioangheraszim <gijohny17@gmail.com>
2024-12-28 00:50:00 +0000

    finished the javascript part have to do the readme

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ let submitBtn = document.getElementById("submit");
 let mailInput = document.getElementById("mailinput");
 let errorMessage = document.getElementById("error");
 let errorIcon = document.getElementById("icon-error");
+let sectionForm = document.querySelector(".section__form");
 
 submitBtn.addEventListener("click", () => {
-    if (mailInput.value !== null) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailValue = mailInput.value.trim();
+
+    if (emailValue === "") {
         errorMessage.style.display = "block";
-        errorIcon.style.display = "block";
-    } else {
+        errorIcon.classList.add("show");
+        sectionForm.classList.add("error");
+    } else if (!emailRegex.test(mailInput.value)) {
+        errorMessage.style.display = "block";
+        errorIcon.classList.add("show");
+    }    
+    else {
         errorMessage.style.display = "none";
-        errorIcon.style.display = "none";
+        errorIcon.classList.remove("show");
+        sectionForm.classList.remove("error");
     }
 });
